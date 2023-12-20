95fd7a3363c12560efd24b96f74c5cc2d41c9ae2
Chris Simpson <lcsimpson@usf.edu>
2023-12-20 12:39:50 -0500

    added error checking and display

--- main.py
+++ main.py
@@ -9,11 +9,14 @@ while True:
    match user_action:
       case 'add':
          todo = input("Enter a todo: ")
-         todos.append(todo.capitalize())
-      case 'show':
+         todos.append(todo)
+      case 'show' | 'display':
          for item in todos:
+            item = item.title()
             print(item)
       case 'exit':
          break
+      case _:
+         print("Unknown command entered")
 
 print('Bye!')
