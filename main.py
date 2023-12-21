5ab96239ae729ee8a30a84ec6f79d8bc3b68de57
Chris Simpson <lcsimpson@usf.edu>
2023-12-21 09:42:05 -0500

    added edit feature

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-user_prompt = "Type add, show, or exit: "
+user_prompt = "Type add, show, edit, or exit: "
 
 todos = []
 
@@ -14,6 +14,11 @@ while True:
          for item in todos:
             item = item.title()
             print(item)
+      case 'edit':
+         number = int(input("Number of the todo to edit: "))
+         number = number - 1
+         new_todo = input("Enter new todo: ")
+         todos[number] = new_todo
       case 'exit':
          break
       case _:
