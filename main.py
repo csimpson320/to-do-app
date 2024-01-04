13b364a95a6ee83ad38758271bf2cc6e072ab653
csimpson320 <chris.simpson462@gmail.com>
2024-01-04 12:16:32 -0500

    added list comprehension

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 user_prompt = "Type add, show, edit, complete, or exit: "
 
 while True:
+   # Get user input and strip space chars from it
    user_action = input(user_prompt)
    user_action = user_action.strip()
 
    match user_action:
       case 'add':
-         todo = input("Enter a todo: ") + "\n"
+         todo = input("Enter a todo: ") + "\n"  # Add newline char for items to be place on newline in file
 
          file = open('todos.txt', 'r')
          todos = file.readlines()
@@ -18,11 +19,13 @@ while True:
          file.writelines(todos)
          file.close()
       case 'show':
+         # Create file object and show current todos items
          file = open('todos.txt', 'r')
          todos = file.readlines()
          file.close()
 
          for index, item in enumerate(todos):
+            item = item.strip('\n')  # Stip newline char so it displays correctly on screen
             row = f"{index + 1}.{item.title()}"
             print(row)
       case 'edit':
